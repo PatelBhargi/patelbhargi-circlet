ddf0e856f06275c5afea7484a08139e0f5095ace
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-29 20:43:16 +0530

    Add files via upload

---
+++ attachments12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int n=5;
+	for(int i=1,p='E';i<=n;i++,p--)
+	{
+		for(int j=1;j<=i;j++)
+		printf((char)p+"" );
+    }
+			printf("\n");
+
+	
+}
+
+	printf("\n");
+		}
+
+	
+	
+}
+
